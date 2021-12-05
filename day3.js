d0d6d18c4e9b14cae6c001f0e9c6c1663c67d458
anton-nystrom <nystromanton00@gmail.com>
2021-12-05 23:08:47 +0100

    first commit

---
+++ day3.js
@@ -0,0 +1,14 @@
+import { input } from "./puzzleInput.js";
+let instructions = input;
+instructions = instructions.split("\n");
+
+console.log("Part One: " + partOne());
+//console.log("Part Two: " + partTwo());
+
+function partOne() {
+    
+};
+
+function partTwo() {
+    
+};
